025b9ae27c3beeada7dd970311f4a8af0972878e
Chris Owen <c.ornine@gmail.com>
2025-03-28 17:35:03 +0800

    feat: split data into groups, show as different coloured paths

--- src/firstChart.js
+++ src/firstChart.js
@@ -1,6 +1,7 @@
 import * as d3 from "d3";
 import allProductsString from "./testdata/allProductsString";
 
+
 const allProductsData = d3.csvParse(allProductsString)
 
 export function renderChart(containerElement) {
@@ -13,6 +14,7 @@ export function renderChart(containerElement) {
   const marginBottom = 30;
   const marginLeft = 40;
 
+  // const to use to define the property we are charting (could be passed into function)
   const PROPERTY = "notional"
 
   // Declare the x (horizontal position) scale.
@@ -27,18 +29,28 @@ export function renderChart(containerElement) {
   const y = d3.scaleLinear()
       // .domain([0, 100])
       //? using the notional, without summing
-      // .domain([0, d3.max(allProductsData, d => d[PROPERTY])])
+      .domain([0, d3.max(allProductsData, d => d[PROPERTY])])
       //? using the notional summed
-      .domain([0, d3.sum(allProductsData, d => d[PROPERTY])])
+      // .domain([0, d3.sum(allProductsData, d => d[PROPERTY])])
       .range([height - marginBottom, marginTop]);
+
+  // Create a colour scale to assign different paths to
+  const colourSelect = d3.scaleLinear()
+    .domain([0,2])
+    .range(["red", "yellow"])
 
   // Create a mapped version of the data with a new property holding the running sum - this way it can be used for both the line and the dots
   const summedPropertyData = allProductsData.map((obj,i,arr) => ({...obj, summed_property: d3.cumsum(arr, d => d[PROPERTY])[i]}))
 
+  // we want to split the data and then create a line for each, and have the summed value available on each group for both lines and dots
+  const mappedData = d3.rollup(allProductsData, v => v, d => d.symbol)
+
+
   // create a line
   const line = d3.line()
     .x(d => x(d.date_time))
-    .y(d => y(d.summed_property)) 
+    .y(d => y(d[PROPERTY])) 
+    // .y(d => y(d.summed_property)) 
     // .y((_d,i,arr) => y(d3.cumsum(arr, d => d[PROPERTY])[i])) 
 
 
@@ -60,22 +72,45 @@ export function renderChart(containerElement) {
   // Testing adding circles on each point
   svg.append("g")
     .selectAll("circle")
-    .data(summedPropertyData)
+    // .data(summedPropertyData)
+    .data(mappedData.get('BTCUSDT'))
     .join("circle")
       .attr("fill", "none")
       .attr("stroke", "yellow")
       .attr("r", 3)
       .attr("stroke-width", 1)
       .attr("cx", d => x(d.date_time))
-      .attr("cy", d => y(d.summed_property))
-      
+      .attr("cy", d => y(d[PROPERTY]))
+
+    // Test adding text at the same points  
+    // svg.append("g")
+    // .selectAll("text")
+    // .data(summedPropertyData)
+    // .join("text")
+    //   .attr("x", d => x(d.date_time))
+    //   .attr("y", d => y(d.summed_property))
+    //   .style("font", d => d.symbol === "BTCUSDT" ? "bold 10px sans-serif" : "bold 15px serif")
+    //   .style("fill", "gold")
+    //   .text(d => d.exchange)    
 
   // Add the line
-  svg.append("path")
-    .attr("fill", "none")
-    .attr("stroke", "chartreuse")
-    .attr("stroke-width", 1.5)
-    .attr("d", line(summedPropertyData))
+  // svg.append("path")
+  //   .attr("fill", "none")
+  //   .attr("stroke", "chartreuse")
+  //   .attr("stroke-width", 1.5)
+  //   .attr("d", line(summedPropertyData))
+
+  const logger = (value) => console.log(value);
+
+  svg.append("g")
+    .selectAll("path")
+    .data([...mappedData.values()])
+    .join("path")
+      .attr("fill", "none")
+      .attr("stroke", (_d,i) => colourSelect(i))
+      .attr("stroke-width", 1.5)
+      .attr("d", line)
+
 
 
   // Append the SVG element.
@@ -107,7 +142,11 @@ console.log(d3.csvParse(allProductsString));
 
 console.log(d3.line() );
 
+const mappedData = d3.rollup(allProductsData, v => v, d => d.symbol)
 
+console.log(mappedData);
+
+// console.log(d3.flatRollup(allProductsData, v => v, d => d.symbol));
 
 
 
